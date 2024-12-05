53f25f40eceb186220185c5e51435b040b3d57ab
Tom Rowbotham <tom.rowbotham@live.co.uk>
2024-12-05 12:10:02 +0000

    Add trust proxy for rate limiter

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const routes = require('./routes');
 const app = express();
 const port = 3001;
 
+// Add trust proxy setting before other middleware
+app.set('trust proxy', 1);
+
 // Enable CORS and JSON parsing
 app.use(cors());
 app.use(express.json());
