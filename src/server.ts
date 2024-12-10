833ef71d48c17ff273d0734c6980828c85fa2af7
Tom Rowbotham <tom.rowbotham@live.co.uk>
2024-12-10 14:38:51 +0000

    Add /health

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import healthRoutes from './routes/healthRoutes';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/sign-in', authRoutes);
+app.use('/health', healthRoutes);
 
 if (require.main === module) {
     const PORT = process.env.PORT || 3000;
